d88cf36510b5d7e0e2dee1ace4e72ccee8dff698
Huang ChenGang <hcggang@163.com>
2021-02-21 22:30:57 +0800

    init project

---
+++ src/main/java/com/jn/xingdaba/order/JnXingdabaOrderApplication.java
@@ -0,0 +1,13 @@
+package com.jn.xingdaba.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JnXingdabaOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JnXingdabaOrderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jn/xingdaba/order/JnXingdabaOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jn.xingdaba.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JnXingdabaOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
